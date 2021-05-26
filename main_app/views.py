30c4109d70c3c4591c3fb8028fec6a411483e7e3
Alexey Volegov <anvolegov87@gmail.com>
2021-05-26 14:46:56 +0300

    add modal (bootstrap) add customer profile

--- main_app/views.py
+++ main_app/views.py
@@ -5,7 +5,7 @@ from django.http import HttpResponseRedirect
 from django.contrib import messages
 from django.contrib.auth import authenticate, login
 
-from .models import Category,  Customer, CartProduct, Product
+from .models import Category, Customer, CartProduct, Product, Order
 from .mixins import CartMixin
 from .forms import OrderForm, LoginForm, RegistrationForm
 from .utils import recalculate_cart
@@ -199,6 +199,16 @@ class RegistrationView(CartMixin, View):
             return HttpResponseRedirect('/')
         context = {'form': form, 'cart': self.cart}
         return render(request, 'registration.html', context)
+
+
+class ProfileView(CartMixin, View):
+
+    def get(self, request, *args, **kwargs):
+        customer = Customer.objects.get(user=request.user)
+        orders = Order.objects.filter(customer=customer).order_by('-created_at')
+        categories = Category.objects.all()
+        context = {'orders': orders, 'cart': self.cart, 'categories': categories}
+        return render(request, 'profile.html', context)
 
 
 
